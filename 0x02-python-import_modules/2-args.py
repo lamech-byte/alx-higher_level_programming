3e7fb031b0e02f1c07184938640ccc834d7d1ef4
Lamech M <79203512+lamech-byte@users.noreply.github.com>
2022-12-01 21:49:30 +0300

    Update 2-args.py

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,43 +1,11 @@
 #!/usr/bin/python3
-def add(a, b):
-    """My addition function
-    Args:
-        a: first integer
-        b: second integer
-    Returns:
-        The return value. a + b
-    """
-    return (a + b)
-
-
-def sub(a, b):
-    """My subtraction function
-    Args:
-        a: first integer
-        b: second integer
-    Returns:
-        The return value. a - b
-    """
-    return (a - b)
-
-
-def mul(a, b):
-    """My multiplication function
-    Args:
-        a: first integer
-        b: second integer
-    Returns:
-        The return value. a * b
-    """
-    return (a * b)
-
-
-def div(a, b):
-    """My division function
-    Args:
-        a: first integer
-        b: second integer
-    Returns:
-        The return value. a / b
-    """
-    return int(a / b)
+if __name__ == "__main__":
+    from sys import argv
+    userin = argv[1:]
+    size = len(userin)
+    print("{:d} {:s}{:s}".
+          format(size,
+                 "arguments" if (size) is not 1 else "argument",
+                 "." if (size) is 0 else ":"))
+    for idx, arg in enumerate(userin):
+        print("{:d}: {:s}".format(idx + 1, arg))
