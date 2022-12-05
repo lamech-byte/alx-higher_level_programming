192abdf6179ae61e905e7c7bc15a16191d14bd81
Lamech M <79203512+lamech-byte@users.noreply.github.com>
2022-12-05 22:14:33 +0300

    Update 6-print_matrix_integer.py

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,1 +1,12 @@
-i
+#!/usr/bin/python3
+def print_matrix_integer(matrix=[[]]):
+    for row in matrix:
+        for i in row:
+            if i is not row[len(row) - 1]:
+                print("{:d}".format(i), end=" ")
+            else:
+                print("{:d}".format(i), end="")
+        print()
+
+# for row in matrix:
+#     print(" ".join("{:d}".format(i) for i in row))
