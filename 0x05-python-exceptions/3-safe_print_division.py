c2b4c4d555aa506ec9367d99eabd7165dd43c279
Lamech M <79203512+lamech-byte@users.noreply.github.com>
2022-12-19 16:31:52 +0300

    Update 3-safe_print_division.py

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -1,1 +1,9 @@
-i
+#!/usr/bin/python3
+def safe_print_division(a, b):
+    try:
+        div = a / b
+    except (ZeroDivisionError, TypeError):
+        div = None
+    finally:
+        print("Inside result: {}".format(div))
+    return div
