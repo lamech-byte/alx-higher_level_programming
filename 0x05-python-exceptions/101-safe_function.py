4ddb28a014b1e16903824bdf436f33e66e2bf8f1
Lamech M <79203512+lamech-byte@users.noreply.github.com>
2022-12-19 16:28:59 +0300

    Update 101-safe_function.py

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -1,1 +1,10 @@
-i
+#!/usr/bin/python3
+from sys import stderr
+
+
+def safe_function(fct, *args):
+    try:
+        return fct(*args)
+    except (ZeroDivisionError, IndexError, TypeError, ValueError) as e:
+        stderr.write("Exception: {}\n".format(e))
+        return None
