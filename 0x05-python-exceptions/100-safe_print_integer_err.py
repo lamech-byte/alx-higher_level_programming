081435694cc5f3f4f81812ce60205f0316eee9ca
Lamech M <79203512+lamech-byte@users.noreply.github.com>
2022-12-19 16:27:54 +0300

    Update 100-safe_print_integer_err.py

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -1,1 +1,11 @@
-i
+#!/usr/bin/python3
+from sys import stderr
+
+
+def safe_print_integer_err(value):
+    try:
+        print("{:d}".format(value))
+        return (True)
+    except (TypeError, ValueError) as e:
+        stderr.write("Exception: {}\n".format(e))
+        return (False)
