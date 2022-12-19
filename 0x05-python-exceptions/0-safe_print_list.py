4ab11937ef215b44367ae70227ecf4e1b20cb6fd
Lamech M <79203512+lamech-byte@users.noreply.github.com>
2022-12-19 14:58:02 +0300

    Update 0-safe_print_list.py

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
-    try:
-        i = 0
-        while i < x:
-            print(my_list[i], end=' ')
-            i += 1
-        print()
-    except IndexError:
-        pass
-    return i
+    idx = 0
+    while idx < x:
+        try:
+            print("{}".format(my_list[idx]), end="")
+        except IndexError:
+            break
+        idx += 1
+    print("")
+    return idx
