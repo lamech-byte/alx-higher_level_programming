d3e5d8fc34fd0e5a30ef12a07d7cbefde20b690b
Lamech M <79203512+lamech-byte@users.noreply.github.com>
2023-01-03 21:47:18 +0300

    Update 101-nqueens.py

--- 0x08-python-more_classes/101-nqueens.py
+++ 0x08-python-more_classes/101-nqueens.py
@@ -1,1 +1,51 @@
-i
+import sys
+
+def solve_nqueens(n, board, col):
+    if col >= n:
+        return True
+    for i in range(n):
+        if is_safe(board, i, col, n):
+            board[i][col] = 1
+            if solve_nqueens(n, board, col + 1):
+                return True
+            board[i][col] = 0
+    return False
+
+def is_safe(board, row, col, n):
+    for i in range(col):
+        if board[row][i] == 1:
+            return False
+    for i, j in zip(range(row, -1, -1), range(col, -1, -1)):
+        if board[i][j] == 1:
+            return False
+    for i, j in zip(range(row, n, 1), range(col, -1, -1)):
+        if board[i][j] == 1:
+            return False
+    return True
+
+def print_solution(board, n):
+    for i in range(n):
+        for j in range(n):
+            print(board[i][j], end=' ')
+        print()
+
+def main():
+    if len(sys.argv) != 2:
+        print("Usage: nqueens N")
+        sys.exit(1)
+    try:
+        n = int(sys.argv[1])
+    except ValueError:
+        print("N must be a number")
+        sys.exit(1)
+    if n < 4:
+        print("N must be at least 4")
+        sys.exit(1)
+    board = [[0 for i in range(n)] for j in range(n)]
+    if solve_nqueens(n, board, 0):
+        print_solution(board, n)
+    else:
+        print("No solution")
+
+if __name__ == "__main__":
+    main()
