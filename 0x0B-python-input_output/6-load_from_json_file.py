fa389fe313722d0f958ae8a8ac01a59ed458839a
Lamech M <79203512+lamech-byte@users.noreply.github.com>
2023-01-10 22:20:24 +0300

    Update 6-load_from_json_file.py

--- 0x0B-python-input_output/6-load_from_json_file.py
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -1,1 +1,17 @@
-i
+#!/usr/bin/python3
+"""
+Module 6-from_json_string
+Contains function that returns python data structure from JSON string
+"""
+
+
+def from_json_string(my_str):
+    """Returns python data structure from JSON string
+    Args:
+        my_str: json string representation
+    Return:
+        python object
+    """
+    import json
+
+    return json.loads(my_str)
