9958b3ecf1ddcf6ae1291087f8befa777f0a09f8
Lamech M <79203512+lamech-byte@users.noreply.github.com>
2023-01-10 22:52:40 +0300

    Update 4-main.py

--- 0x0B-python-input_output/main_files/4-main.py
+++ 0x0B-python-input_output/main_files/4-main.py
@@ -1,1 +1,5 @@
-i
+#!/usr/bin/python3
+append_write = __import__('4-append_write').append_write
+
+nb_characters_added = append_write("file_append.txt", "Alx is so cool!\n")
+print(nb_characters_added)
