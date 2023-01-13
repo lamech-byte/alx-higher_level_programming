e9a3b46a0aa1b060c7c219b51b29c8ee9b6cd4dc
Lamech M <79203512+lamech-byte@users.noreply.github.com>
2023-01-13 10:47:23 +0300

    Update 10-main.py

--- 0x0B-python-input_output/main_files/10-main.py
+++ 0x0B-python-input_output/main_files/10-main.py
@@ -1,12 +1,42 @@
 #!/usr/bin/python3
-MyClass = __import__('10-my_class').MyClass
-class_to_json = __import__('10-class_to_json').class_to_json
+import os
+import sys
 
-m = MyClass("John")
-m.number = 89
-print(type(m))
-print(m)
+Student = __import__('10-student').Student
+read_file = __import__('0-read_file').read_file
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
-mj = class_to_json(m)
-print(type(mj))
-print(mj)
+path = sys.argv[1]
+
+if os.path.exists(path):
+    os.remove(path)
+
+student_1 = Student("John", "Doe", 23)
+j_student_1 = student_1.to_json()
+print("Initial student:")
+print(student_1)
+print(type(student_1))
+print(type(j_student_1))
+print("{} {} {}".format(student_1.first_name, student_1.last_name, student_1.age))
+
+
+save_to_json_file(j_student_1, path)
+read_file(path)
+print("\nSaved to disk")
+
+
+print("Fake student:")
+new_student_1 = Student("Fake", "Fake", 89)
+print(new_student_1)
+print(type(new_student_1))
+print("{} {} {}".format(new_student_1.first_name, new_student_1.last_name, new_student_1.age))
+
+
+print("Load dictionary from file:")
+new_j_student_1 = load_from_json_file(path)
+
+new_student_1.reload_from_json(j_student_1)
+print(new_student_1)
+print(type(new_student_1))
+print("{} {} {}".format(new_student_1.first_name, new_student_1.last_name, new_student_1.age))
