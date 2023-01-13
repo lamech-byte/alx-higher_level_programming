0fcfe2f0e92f1bc3683cd64b976189197013efdb
Lamech M <79203512+lamech-byte@users.noreply.github.com>
2023-01-13 14:57:22 +0300

    Update 12-main.py

--- 0x0B-python-input_output/main_files/12-main.py
+++ 0x0B-python-input_output/main_files/12-main.py
@@ -1,13 +1,27 @@
 #!/usr/bin/python3
-Student = __import__('12-student').Student
+"""
+12-main
+"""
+pascal_triangle = __import__('12-pascal_triangle').pascal_triangle
 
-student_1 = Student("John", "Doe", 23)
-student_2 = Student("Bob", "Dylan", 27)
+def print_triangle(triangle):
+    """
+    Print the triangle
+    """
+    for row in triangle:
+        print("[{}]".format(",".join([str(x) for x in row])))
 
-j_student_1 = student_1.to_json()
-j_student_2 = student_2.to_json(['first_name', 'age'])
-j_student_3 = student_2.to_json(['middle_name', 'age'])
 
-print(j_student_1)
-print(j_student_2)
-print(j_student_3)
+if __name__ == "__main__":
+    print("Size -5 shouldn't print anything")
+    print_triangle(pascal_triangle(-5))
+    print("Size 0 shouldn't print anything")
+    print_triangle(pascal_triangle(0))
+    print("Size 1 should print [1]")
+    print_triangle(pascal_triangle(1))
+    print("Size 2 should print [1] , [1, 1]")
+    print_triangle(pascal_triangle(2))
+    print("Size 3 should print [1], [1, 1], [1, 2, 1]")
+    print_triangle(pascal_triangle(3))
+    print("Size 5 should print [1], [1, 1], [1, 2, 1], [1, 3, 3, 1], [1, 4, 6, 4, 1]")
+    print_triangle(pascal_triangle(5))
