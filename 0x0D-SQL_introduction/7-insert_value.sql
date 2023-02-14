e6e9133fe11d0d552883ac6499fa2888a90ab303
Lamech M <79203512+lamech-byte@users.noreply.github.com>
2023-02-14 15:44:31 +0300

    Update 7-insert_value.sql

--- 0x0D-SQL_introduction/7-insert_value.sql
+++ 0x0D-SQL_introduction/7-insert_value.sql
@@ -1,3 +1,2 @@
--- insert a new row of data in this table in this database
-
-INSERT INTO `first_table` (`id`, `name`) VALUES (89, 'Holberton School');
+-- Inserts a new row into the table first_table.
+INSERT INTO `first_table` (`id`, `name`) VALUES (89, "Best School");
