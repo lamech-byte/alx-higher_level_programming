6c20580b5aad65b3e42488abd2384f6abe96c5d3
Lamech M <79203512+lamech-byte@users.noreply.github.com>
2023-03-19 21:53:01 +0300

    Update 8-model_state_fetch_first.py

--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -1,22 +1,31 @@
 #!/usr/bin/python3
-# Prints the first State object from the database hbtn_0e_6_usa.
-# Usage: ./8-model_state_fetch_first.py <mysql username> /
-#                                       <mysql password> /
-#                                       <database name>
-import sys
+"""
+return first state object from database via python
+parameters given to script: username, password, database
+"""
+
+from sys import argv
+from model_state import Base, State
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from model_state import State
+
 
 if __name__ == "__main__":
-    engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}"
-                           .format(sys.argv[1], sys.argv[2], sys.argv[3]),
-                           pool_pre_ping=True)
+
+    # make engine for database
+    user = argv[1]
+    passwd = argv[2]
+    db = argv[3]
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.
+                           format(user, passwd, db), pool_pre_ping=True)
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    state = session.query(State).order_by(State.id).first()
-    if state is None:
-        print("Nothing")
+    # query first python instance in database
+    first_instance = session.query(State).order_by(State.id).first()
+    if first_instance:
+        print("{:d}: {:s}".format(first_instance.id, first_instance.name))
     else:
-        print("{}: {}".format(state.id, state.name))
+        print("Nothing")
+
+    session.close()
