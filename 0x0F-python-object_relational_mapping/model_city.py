f7601544ee6c6b36bd2b1772007529add1e604a1
Lamech M <79203512+lamech-byte@users.noreply.github.com>
2023-03-20 06:45:37 +0300

    Update model_city.py

--- 0x0F-python-object_relational_mapping/model_city.py
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -1,11 +1,21 @@
 #!/usr/bin/python3
-"""Start link class to table in database 
 """
-import sys
+Defines class City
+"""
+
+from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.ext.declarative import declarative_base
 from model_state import Base, State
 
-from sqlalchemy import (create_engine)
+Base = declarative_base()
 
-if __name__ == "__main__":
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
-    Base.metadata.create_all(engine)
+
+class City(Base):
+    """
+    Class City; instance of Base
+    Linked to MySQL table "city"
+    """
+    __tablename__ = "cities"
+    id = Column(Integer, nullable=False, primary_key=True)  # autoincrements
+    name = Column(String(128), nullable=False)
+    state_id = Column(Integer, ForeignKey(State.id), nullable=False)
