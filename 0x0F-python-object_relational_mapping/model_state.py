58e267109f10d47550b40260dfd5e1471d9f55cf
Lamech M <79203512+lamech-byte@users.noreply.github.com>
2023-03-20 06:40:47 +0300

    Update model_state.py

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -1,20 +1,28 @@
+-- Create database hbtn_0e_6_usa
+CREATE DATABASE
+IF NOT EXISTS hbtn_0e_6_usa;
+USE hbtn_0e_6_usa;
+SHOW
+CREATE TABLE states;
+
+guillaume@ubuntu:
+~/0x0F$ cat 6-model_state.sql | mysql -uroot -p
+Enter
+password:
+ERROR 1146
+(42S02) at line 4: Table 'hbtn_0e_6_usa.states' doesn't exist
+guillaume@ubuntu:~/0x0F$ cat 6-model_state.py
 #!/usr/bin/python3
-# Defines a State model.
-# Inherits from SQLAlchemy Base and links to the MySQL table states.
+"""Start link class to table in database 
+"""
+import sys
+from model_state import Base, State
 
-from sqlalchemy import Column, Integer, String
-from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import (create_engine)
 
-Base = declarative_base()
-
-
-class State(Base):
-    """Represents a state for a MySQL database.
-
-    __tablename__ (str): The name of the MySQL table to store States.
-    id (sqlalchemy.Integer): The state's id.
-    name (sqlalchemy.String): The state's name.
-    """
-    __tablename__ = "states"
-    id = Column(Integer, primary_key=True)
-    name = Column(String(128), nullable=False)
+if __name__ == "__main__":
+    engine = create_engine('mysql+
+mysqldb:
+//{}:{}@localhost/{}'.format(sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+    
