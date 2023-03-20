90afa6be413a8df84c692ff634bdfa34ea3b96fa
Lamech M <79203512+lamech-byte@users.noreply.github.com>
2023-03-20 06:58:13 +0300

    Update 14-model_city_fetch_by_state.py

--- 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -1,21 +1,31 @@
 #!/usr/bin/python3
-'''
-Prints all City objects from the database hbtn_0e_14_usa
-'''
+"""
+return all cities from database via python
+parameters given to script: username, password, database
+"""
+
 from sys import argv
+from model_state import Base, State
+from model_city import City
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from model_state import State
-from model_city import City
 
 
 if __name__ == "__main__":
-    engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}"
-                           .format(argv[1], argv[2], argv[3]))
-    InstanceSession = sessionmaker(bind=engine)
-    session = InstanceSession()
 
-    for state, city in session.query(State, City).join(
-            City).order_by(City.id).all():
-        print("{}: ({}) {}".format(state.name, city.id, city.name))
+    # make engine for database
+    user = argv[1]
+    passwd = argv[2]
+    db = argv[3]
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.
+                           format(user, passwd, db), pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    # query multiple tables in database and print info from tables
+    for q in session.query(State.name, City.id, City.name).filter(
+            State.id == City.state_id).order_by(City.id):
+        print("{:s}: ({:d}) {:s}".format(q[0], q[1], q[2]))
+
     session.close()
