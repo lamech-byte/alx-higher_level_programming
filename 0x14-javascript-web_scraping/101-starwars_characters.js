fdd1dc0bc827bc8f077432b1bb473fc669e62924
Lamech M <79203512+lamech-byte@users.noreply.github.com>
2023-04-25 20:59:00 +0300

    Update 101-starwars_characters.js

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -1,20 +1,39 @@
 #!/usr/bin/node
+
 const request = require('request');
-const url = 'https://swapi.co/api/films/' + process.argv[2];
-request(url, function (error, response, body) {
-  if (!error) {
-    let characters = JSON.parse(body).characters;
-    printCharacters(characters, 0);
+
+const movieId = process.argv[2];
+
+const movieUrl = `https://swapi-api.hbtn.io/api/films/${movieId}`;
+
+request(movieUrl, function (error, response, body) {
+  if (error) {
+    console.error(error);
+    return;
+  }
+
+  const charactersUrls = JSON.parse(body).characters;
+  const charactersNames = [];
+
+  let completedRequests = 0;
+
+  for (const url of charactersUrls) {
+    request(url, function (error, response, body) {
+      if (error) {
+        console.error(error);
+        return;
+      }
+
+      const characterName = JSON.parse(body).name;
+      charactersNames.push(characterName);
+
+      completedRequests++;
+
+      if (completedRequests === charactersUrls.length) {
+        for (const name of charactersNames) {
+          console.log(name);
+        }
+      }
+    });
   }
 });
-
-function printCharacters (characters, index) {
-  request(characters[index], function (error, response, body) {
-    if (!error) {
-      console.log(JSON.parse(body).name);
-      if (index + 1 < characters.length) {
-        printCharacters(characters, index + 1);
-      }
-    }
-  });
-}
