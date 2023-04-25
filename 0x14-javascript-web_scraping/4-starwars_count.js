dc123d1a45b4a3ce079ede59f1d8b57dd403935a
Lamech M <79203512+lamech-byte@users.noreply.github.com>
2023-04-25 20:40:57 +0300

    Update 4-starwars_count.js

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,14 +1,12 @@
 #!/usr/bin/node
 const request = require('request');
-const apiUrl = process.argv[2];
-const characterId = "18";
-
-request(apiUrl, function (error, response, body) {
-  if (error) {
-    console.log(error);
-  } else {
-    const movies = JSON.parse(body).results;
-    const count = movies.filter(movie => movie.characters.includes(`https://swapi-api.alx-tools.com/api/people/${characterId}/`)).length;
-    console.log(count);
+request(process.argv[2], function (error, response, body) {
+  if (!error) {
+    const results = JSON.parse(body).results;
+    console.log(results.reduce((count, movie) => {
+      return movie.characters.find((character) => character.endsWith('/18/'))
+        ? count + 1
+        : count;
+    }, 0));
   }
 });
