fe920fd4f8a499f7d0f0d480e7ac5556b3ab4cc0
Lamech M <79203512+lamech-byte@users.noreply.github.com>
2023-04-25 20:05:15 +0300

    Update 0-readme.js

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,12 +1,9 @@
 #!/usr/bin/node
 const fs = require('fs');
-
-const filePath = process.argv[2];
-
-fs.readFile(filePath, 'utf-8', (error, content) => {
-  if (error) {
-    console.error(error);
-    return;
+fs.readFile(process.argv[2], 'utf8', function (err, contents) {
+  if (contents === undefined) {
+    console.log(err);
+  } else {
+    console.log(contents);
   }
-  console.log(content);
 });
