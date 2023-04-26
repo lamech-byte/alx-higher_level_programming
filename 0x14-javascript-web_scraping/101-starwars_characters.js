cca819c3397a58d37b1d532d015ca7aba8274b8e
Lamech M <79203512+lamech-byte@users.noreply.github.com>
2023-04-26 12:05:24 +0300

    Update 101-starwars_characters.js

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -1,22 +1,35 @@
 #!/usr/bin/node
 
 const request = require('request');
-const movieId = process.argv[2];
-const url = `https://swapi-api.hbtn.io/api/films/${movieId}`;
 
-request(url, (error, response, body) => {
-  if (error) {
-    console.log(error);
-  } else {
-    const characters = JSON.parse(body).characters;
-    characters.forEach((charUrl) => {
-      request(charUrl, (charError, charResponse, charBody) => {
-        if (charError) {
-          console.log(charError);
-        } else {
-          console.log(JSON.parse(charBody).name);
-        }
-      });
-    });
+const movieId = process.argv[2];
+const baseUrl = 'https://swapi.dev/api';
+const filmUrl = `${baseUrl}/films/${movieId}/`;
+const charactersUrl = `${baseUrl}/people/`;
+
+// Get list of characters in correct order
+request.get(`${baseUrl}/films/`, (err, res, body) => {
+  if (err) {
+    console.error(err);
+    return;
   }
+  const films = JSON.parse(body).results;
+  const characters = films.find(f => f.url === filmUrl).characters;
+  const characterNames = new Array(characters.length);
+
+  // Get character names in correct order
+  characters.forEach((charUrl, index) => {
+    request.get(charUrl, (err, res, body) => {
+      if (err) {
+        console.error(err);
+        return;
+      }
+      characterNames[index] = JSON.parse(body).name;
+
+      // Print character names when all have been retrieved
+      if (characterNames.filter(Boolean).length === characters.length) {
+        characterNames.forEach(name => console.log(name));
+      }
+    });
+  });
 });
