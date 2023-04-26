ec7291bb8d85f7363dd242154efa0ff4558cc057
Lamech M <79203512+lamech-byte@users.noreply.github.com>
2023-04-26 11:18:08 +0300

    Update 0-readme.js

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,6 +1,12 @@
- if (err) {
-    console.log(err);
-  } else {
-    console.log(data);
+#!/usr/bin/node
+const fs = require('fs');
+
+const filePath = process.argv[2];
+
+fs.readFile(filePath, 'utf-8', (err, data) => {
+  if (err) {
+    console.error(err);
+    return;
   }
+  console.log(data);
 });
