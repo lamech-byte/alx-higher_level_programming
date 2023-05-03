73473ec27c2b3c7d53accb276761e63893e32675
Lamech M <79203512+lamech-byte@users.noreply.github.com>
2023-05-03 09:16:28 +0300

    0x15-javascript-web_jquery

--- 0x15-javascript-web_jquery/8-script.js
+++ 0x15-javascript-web_jquery/8-script.js
@@ -1,1 +1,8 @@
-
+$(document).ready(function () {
+  $.get('https://swapi-api.alx-tools.com/api/films/?format=json', function (data) {
+    const movies = data.results;
+    for (let i = 0; i < movies.length; i++) {
+      $('<li>' + movies[i].title + '</li>').appendTo('#list_movies');
+    }
+  });
+});
